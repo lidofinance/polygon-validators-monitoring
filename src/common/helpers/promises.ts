1564750839040a0514cbb847c0f4cb761370b4a3
madlabman <10616301+madlabman@users.noreply.github.com>
2023-01-16 08:51:43 +0100

    fix: join errors with \n

--- src/common/helpers/promises.ts
+++ src/common/helpers/promises.ts
@@ -3,7 +3,7 @@ export async function allSettled<T>(promises: Promise<T>[]): Promise<T[]> {
   const results = await Promise.allSettled(promises);
   const failed = results.filter((r) => r.status === 'rejected');
   if (failed.length > 0) {
-    throw new Error(failed.map((r: any) => r.reason).join(''));
+    throw new Error(failed.map((r: any) => r.reason).join('\n'));
   }
 
   return results.map((r: any) => r.value) as T[];
