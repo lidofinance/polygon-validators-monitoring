a0b1141e371a15d220b90b08d674aa97b907445e
madlabman <10616301+madlabman@users.noreply.github.com>
2023-01-16 11:41:12 +0100

    fix: return back averages metrics

--- src/metrics/metrics.consts.ts
+++ src/metrics/metrics.consts.ts
@@ -1,4 +1,6 @@
 export const VALIDATORS_PERF_BENCHMARK = 'vals_perf_benchmark';
+export const VALIDATORS_PERF_AVG_RATE = 'vals_perf_avg_rate';
+export const VALIDATORS_PERF_TOP_RATE = 'vals_perf_top_rate';
 export const VALIDATORS_PERF_INDEX = 'vals_perf_idx';
 export const VALIDATORS_PERF_CMP = 'vals_perf_cmp';
 
