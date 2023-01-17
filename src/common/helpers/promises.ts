4fb9bbc8006e050ecc7a282b6e64f51e9fe85d78
madlabman <10616301+madlabman@users.noreply.github.com>
2023-01-17 08:51:05 +0100

    refactor: use AggregateError instead of Error

--- src/common/helpers/promises.ts
+++ src/common/helpers/promises.ts
@@ -3,7 +3,10 @@ export async function allSettled<T>(promises: Promise<T>[]): Promise<T[]> {
   const results = await Promise.allSettled(promises);
   const failed = results.filter((r) => r.status === 'rejected');
   if (failed.length > 0) {
-    throw new Error(failed.map((r: any) => r.reason).join('\n'));
+    throw new global.AggregateError(
+      failed.map((r: any) => r.reason),
+      failed.map((r: any) => r.reason.message).join('\n'),
+    );
   }
 
   return results.map((r: any) => r.value) as T[];
