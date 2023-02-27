ed8e77001deb423116a16c146e9737c79389c995
madlabman <10616301+madlabman@users.noreply.github.com>
2023-02-27 11:50:43 +0100

    chore: remove unused method

--- src/worker/worker.service.ts
+++ src/worker/worker.service.ts
@@ -23,10 +23,8 @@ import {
   CHECKPOINTS_TO_KEEP,
   DRY_RUN_WINDOW,
   MAIN_JOB_NAME,
-  MAX_EXPECTED_BLOCK_FREQUENCY_SECONDS,
   METRICS_COMPUTE_BATCH_SIZE,
   METRICS_RETENTION_JOB_NAME,
-  SECS_PER_BLOCK,
   STAKE_EVENTS_JOB_NAME,
   STALE_CHECKPOINT_THRESHOLD,
 } from './worker.consts';
@@ -325,19 +323,6 @@ export class WorkerService implements OnModuleInit {
     }
 
     this.logger.log('Monikers updated');
-  }
-
-  /**
-   * Check if the given block number is stale
-   * relative to the fetched latest one
-   */
-  private isStaleBlock(blockNumber: number): boolean {
-    if (!this.latestBlockNumber) return false;
-
-    return (
-      this.latestBlockNumber - blockNumber * SECS_PER_BLOCK >
-      MAX_EXPECTED_BLOCK_FREQUENCY_SECONDS
-    );
   }
 
   /**
